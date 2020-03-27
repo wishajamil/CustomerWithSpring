a0271b39fe1635d20ea0d5ed5c6553d8387c5be7
wishajamil <45885029+wishajamil@users.noreply.github.com>
2020-03-27 17:09:44 -0400

    Update MavenWrapperDownloader.java

--- .mvn/wrapper/MavenWrapperDownloader.java
+++ .mvn/wrapper/MavenWrapperDownloader.java
@@ -19,7 +19,7 @@ import java.io.*;
 import java.nio.channels.*;
 import java.util.Properties;
 
-public class MavenWrapperDownloader {
+public class MavenWrapperDownloader { 
 
     private static final String WRAPPER_VERSION = "0.5.6";
     /**
